c1a0c1576c6dda2d3310fd82e6c91f164abca273
julienb86 <julien.bouafia@gmail.com>
2019-11-06 10:29:40 +0100

    added signin component and formInput component and styled buttons

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Signin from "../../components/signin/signin.component";
+import "./sign-in-and-sign-up.styles.scss";
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <Signin />
+    </div>
+  );
+};
+export default SignInAndSignUpPage;
