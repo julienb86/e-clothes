b1c6baa39f783373d1cd01aab6c91c70ab68f232
julienb86 <julien.bouafia@gmail.com>
2019-11-12 16:04:47 +0100

    added firebase signup authentification and styled components

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Signin from "../../components/signin/signin.component";
+import Signup from "../../components/sign-up/sign-up.component";
 import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUpPage = () => {
   return (
     <div className="sign-in-and-sign-up">
       <Signin />
+      <Signup />
     </div>
   );
 };
